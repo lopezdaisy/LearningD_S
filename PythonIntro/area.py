f51cce999bf3feb2b32e8bc2dbfdec0e6b3ff597
lopezdaisy <lopez.daisy2022@gmail.com>
2023-12-19 14:07:57 +0300

    PythonIntro:modified

---
+++ PythonIntro/area.py
@@ -0,0 +1,7 @@
+def  calculate_area(base,height):
+    print("__name__: ", __name__)
+    return 1/2*(base*height)
+if __name__ == "__main__" :
+    a = calculate_area(10,20)
+    print("I am in area.py")
+    print("area: ",a)
